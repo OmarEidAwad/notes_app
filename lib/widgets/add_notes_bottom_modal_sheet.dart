532cb91f3f0abd78c25e83b99ed3d5c11341eb1c
OmarEidAwad <152607793+OmarEidAwad@users.noreply.github.com>
2026-01-28 00:30:54 +0300

    add custom button and edit notes view

--- lib/widgets/add_notes_bottom_modal_sheet.dart
+++ lib/widgets/add_notes_bottom_modal_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/widgets/custom_button.dart';
 import 'package:notesapp/widgets/custom_text_feld.dart';
 
 class AddBottomModalSheet extends StatelessWidget {
@@ -7,13 +8,18 @@ class AddBottomModalSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: const [
-          SizedBox(height: 32),
-          CustomTextField(hint: 'title'),
-          SizedBox(height: 16),
-          CustomTextField(hint: 'content', maxLines: 5),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: const [
+            SizedBox(height: 32),
+            CustomTextField(hint: 'title'),
+            SizedBox(height: 16),
+            CustomTextField(hint: 'content', maxLines: 5),
+            SizedBox(height: 32),
+            CustomButton(),
+            SizedBox(height: 55),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/views/edit_notes_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({Key? key}) : super(key: key);
@@ -7,43 +8,55 @@ class NoteItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
-      child: Container(
-        decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text(
-                'Flutter Tips',
-                style: TextStyle(fontSize: 26, color: Colors.black),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                  'Build your career with tharwat samy',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.black.withOpacity(.4),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return EditNotesView();
+              },
+            ),
+          );
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          padding: const EdgeInsets.only(left: 16, top: 24, bottom: 24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Text(
+                  'Flutter Tips',
+                  style: TextStyle(fontSize: 26, color: Colors.black),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Text(
+                    'Build your career with tharwat samy',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black.withOpacity(.4),
+                    ),
                   ),
                 ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.delete, color: Colors.black, size: 35),
+                ),
               ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.delete, color: Colors.black, size: 35),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Text(
+                  'May 21,2022',
+                  style: TextStyle(color: Colors.black.withOpacity(.4)),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: Text(
-                'May 21,2022',
-                style: TextStyle(color: Colors.black.withOpacity(.4)),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
